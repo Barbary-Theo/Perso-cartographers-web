f6b8e523a3b7dc71a69e880094950b0da90e1291
Barbary-Theo <theo.barbary25@gmail.com>
2022-01-21 20:25:41 +0100

    init prohect

---
+++ src/main/java/cartographers/game/Controller/MainController.java
@@ -0,0 +1,18 @@
+package cartographers.game.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String getMappingPage(Model model){
+        return "main";
+    }
+
+}
